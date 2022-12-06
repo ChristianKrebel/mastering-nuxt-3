465f75739ce54e11a9dbd3def61e7aeb1d0ead1f
Christian Krebel <krebel@smartsquare.de>
2022-12-06 20:26:13 +0100

    Use local storage for completion state and force client only use of its button

--- pages/course/chapters/[chaptersSlug]/lessons/[lessonsSlug].vue
+++ pages/course/chapters/[chaptersSlug]/lessons/[lessonsSlug].vue
@@ -2,21 +2,25 @@
   <h1 class="text-lg font-black text-emerald-600">{{ chapter.title }}</h1>
   <h2 class="mt-0 text-xxl font-bold">{{ lesson.title }}</h2>
   <video-player v-if="lesson.videoId" :video-id="lesson.videoId" class="my-4"/>
-  <complete-button :is-completed="isLessonCompleted" @update:isCompleted="toggleCompletion" />
-  <NuxtLink v-if="lesson.downloadUrl" :to="lesson.downloadUrl">
-    <button
-        class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 mr-4 hover:bg-orange-200 hover:text-orange-800"
-    >
-      Download Video
-    </button>
-  </NuxtLink>
-  <NuxtLink v-if="lesson.sourceUrl" :to="lesson.sourceUrl">
-    <button
-        class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 hover:bg-orange-200 hover:text-orange-800"
-    >
-      Download Source
-    </button>
-  </NuxtLink>
+  <template class="flex justify-between">
+    <complete-button :is-completed="isLessonCompleted" @update:isCompleted="toggleCompletion"/>
+    <div>
+      <NuxtLink v-if="lesson.downloadUrl" :to="lesson.downloadUrl">
+        <button
+            class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 hover:bg-orange-200 hover:text-orange-800"
+        >
+          Download Video
+        </button>
+      </NuxtLink>
+      <NuxtLink v-if="lesson.sourceUrl" :to="lesson.sourceUrl">
+        <button
+            class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 ml-4 hover:bg-orange-200 hover:text-orange-800"
+        >
+          Download Source
+        </button>
+      </NuxtLink>
+    </div>
+  </template>
   <p>{{ lesson.text }}</p>
 </template>
 
@@ -31,7 +35,7 @@ const lesson = computed(() => {
   return chapter.value.lessons.find((it) => it.slug === route.params.lessonsSlug)
 })
 
-const progress = useState("progress", () => ([]))
+const progress = useLocalStorage("progress", () => ([]))
 
 const isLessonCompleted = computed(() => {
   if (!progress.value[chapter.value.number - 1]
