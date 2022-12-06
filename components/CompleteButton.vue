5d2f3afc84e27aa57c2a27bb6980ca57cc6542c3
Christian Krebel <krebel@smartsquare.de>
2022-12-06 12:06:06 +0100

    Add lesson completion button

---
+++ components/CompleteButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <button
+        @click="isLessonCompleted = !isLessonCompleted"
+        class="rounded-xl px-4 py-1 mr-4"
+        :class="isLessonCompleted ? 'completed' : 'notCompleted'"
+    >
+      {{ isLessonCompleted ? "Lesson Completed" : "Complete Lesson" }}
+    </button>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  isCompleted: {
+    type: Boolean,
+    required: true
+  }
+})
+
+const emit = defineEmits(['update:isCompleted'])
+
+const isLessonCompleted = computed({
+  get() {
+    return props.isCompleted
+  },
+  set(value) {
+    emit('update:isCompleted', value)
+  }
+})
+</script>
+
+<style scoped>
+.completed {
+  @apply bg-emerald-300 text-emerald-900 hover:bg-emerald-200 hover:text-emerald-800;
+}
+
+.notCompleted {
+  @apply bg-gray-300 text-gray-900 hover:bg-gray-200 hover:text-gray-800;
+}
+</style>
--- pages/course/chapters/[chaptersSlug]/lessons/[lessonsSlug].vue
+++ pages/course/chapters/[chaptersSlug]/lessons/[lessonsSlug].vue
@@ -1,12 +1,21 @@
 <template>
   <h1 class="text-lg font-black text-emerald-600">{{ chapter.title }}</h1>
   <h2 class="mt-0 text-xxl font-bold">{{ lesson.title }}</h2>
-  <video-player v-if="lesson.videoId" :video-id="lesson.videoId" class="my-4" />
+  <video-player v-if="lesson.videoId" :video-id="lesson.videoId" class="my-4"/>
+  <complete-button :is-completed="isLessonCompleted" @update:isCompleted="toggleCompletion" />
   <NuxtLink v-if="lesson.downloadUrl" :to="lesson.downloadUrl">
-    <button class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 mr-4 hover:bg-orange-200 hover:text-orange-800">Download Video</button>
+    <button
+        class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 mr-4 hover:bg-orange-200 hover:text-orange-800"
+    >
+      Download Video
+    </button>
   </NuxtLink>
   <NuxtLink v-if="lesson.sourceUrl" :to="lesson.sourceUrl">
-    <button class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 hover:bg-orange-200 hover:text-orange-800">Download Source</button>
+    <button
+        class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 hover:bg-orange-200 hover:text-orange-800"
+    >
+      Download Source
+    </button>
   </NuxtLink>
   <p>{{ lesson.text }}</p>
 </template>
@@ -21,6 +30,12 @@ const chapter = computed(() => {
 const lesson = computed(() => {
   return chapter.value.lessons.find((it) => it.slug === route.params.lessonsSlug)
 })
+
+const isLessonCompleted = ref(false)
+
+const toggleCompletion = () => {
+  isLessonCompleted.value = !isLessonCompleted.value
+}
 
 const title = computed(() => `${lesson.value.title} / ${chapter.value.title} / Mastering Nuxt 3`)
 useHead({
