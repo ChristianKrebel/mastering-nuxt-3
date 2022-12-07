63f35e7c62427fb19d7d8f692693938d78fe6423
Christian Krebel <krebel@smartsquare.de>
2022-12-07 12:55:44 +0100

    Add default layout with footer for information and links

--- pages/course.vue
+++ pages/course.vue
@@ -1,38 +1,34 @@
 <template>
-  <div
-    class="p-12 bg-emerald-400 w-full h-full min-h-screen flex flex-col items-center"
-  >
-    <div class="prose mb-12">
-      <h1>
-        <span class="font-medium">
-          Course:
-          <span class="font-bold">Mastering Nuxt 3</span>
-        </span>
-      </h1>
+  <div class="prose mb-12">
+    <h1>
+      <span class="font-medium">
+        Course:
+        <span class="font-bold">Mastering Nuxt 3</span>
+      </span>
+    </h1>
+  </div>
+
+  <div class="flex flex-row justify-center flex-grow">
+    <div
+        class="prose mr-4 p-10 bg-emerald-50 rounded-3xl min-w-[20ch] max-w-[30ch] flex flex-col"
+    >
+      <h3 class="text-xl">Chapters</h3>
+      <template v-for="chapter in chapters" :key="chapter.slug">
+        <h4>{{ chapter.title }}</h4>
+        <template v-for="(lesson, index) in chapter.lessons" :key="lesson.slug">
+          <NuxtLink :to="lesson.path"
+                    :class="{'bg-white': lesson.path === $route.fullPath}"
+                    class="group -mx-4 mb-1 py-1 px-4 no-underline rounded-xl hover:bg-white">
+            <span class="text-emerald-600 group-hover:text-emerald-800" >{{ `${index + 1}. ` }}</span>
+            <span class="group-hover:text-emerald-900"
+                  >{{ lesson.title }}</span>
+          </NuxtLink>
+        </template>
+      </template>
     </div>
 
-    <div class="flex flex-row justify-center flex-grow">
-      <div
-          class="prose mr-4 p-10 bg-emerald-50 rounded-3xl min-w-[20ch] max-w-[30ch] flex flex-col"
-      >
-        <h3 class="text-xl">Chapters</h3>
-        <template v-for="chapter in chapters" :key="chapter.slug">
-          <h4>{{ chapter.title }}</h4>
-          <template v-for="(lesson, index) in chapter.lessons" :key="lesson.slug">
-            <NuxtLink :to="lesson.path"
-                      :class="{'bg-white': lesson.path === $route.fullPath}"
-                      class="group -mx-4 mb-1 py-1 px-4 no-underline rounded-xl hover:bg-white">
-              <span class="text-emerald-600 group-hover:text-emerald-800" >{{ `${index + 1}. ` }}</span>
-              <span class="group-hover:text-emerald-900"
-                    >{{ lesson.title }}</span>
-            </NuxtLink>
-          </template>
-        </template>
-      </div>
-
-      <div class="prose p-10 bg-white rounded-3xl ">
-        <NuxtPage/>
-      </div>
+    <div class="prose p-10 bg-white rounded-3xl ">
+      <NuxtPage/>
     </div>
   </div>
 </template>
