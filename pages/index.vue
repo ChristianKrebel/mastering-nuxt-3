cca94189d5f173467327b11274e82c59ff90022c
Christian Krebel <krebel@smartsquare.de>
2022-12-07 10:48:06 +0100

    Route from index page to first lesson

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,6 @@
-<template>Hello, world!</template>
+<template></template>
+
+<script lang="ts" setup>
+const router = useRouter()
+router.push("/course/chapters/1-chapter-1/lessons/1-introduction-to-typescript-with-vue-js-3")
+</script>
