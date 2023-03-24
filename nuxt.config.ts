f516c9fee0bb8fdb6c06c97fd1863e3e21415b4f
Christian Krebel <krebel@smartsquare.de>
2023-03-24 20:04:38 +0100

    Add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import eslintPlugin from "vite-plugin-eslint"
 
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/supabase"],
   vite: {
     plugins: [eslintPlugin()],
   },
