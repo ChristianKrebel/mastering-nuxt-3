ef1cea3ce35ab0e2a09095c546f74b2b2abfe470
Christian Krebel <krebel@smartsquare.de>
2023-03-31 18:23:14 +0200

    Add AccountCard for user info and login + logout

---
+++ components/AccountCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="px-4 bg-emerald-50 rounded-3xl flex items-center space-x-3 h-16 min-w-fit group">
+    <template v-if="user">
+      <img class="rounded-full w-12 h-12" :src="avatarUrl" alt="profileImage" />
+      <div class="text-right pr-2">
+        <div class="font-medium text-base">{{ userFullName }}</div>
+        <div class="font-normal text-xs text-slate-500 group-hover:hidden">{{ `@${userName}` }}</div>
+        <div
+          class="font-bold text-xs text-black hover:text-emerald-900 hidden group-hover:block cursor-pointer"
+          @click="logout"
+        >
+          Log out
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <button class="text-black rounded-xl px-4 py-1 hover:bg-white hover:text-emerald-900" @click="login">
+        Log in with GitHub
+      </button>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const user = useSupabaseUser()
+const sb = useSupabaseClient()
+
+const userFullName = computed(() => user.value?.user_metadata?.full_name)
+const userName = computed(() => user.value?.user_metadata?.user_name)
+const avatarUrl = computed(() => user.value?.user_metadata?.avatar_url)
+
+const login = async () => {
+  const { error } = await sb.auth.signInWithOAuth({
+    provider: "github",
+  })
+
+  if (error) {
+    console.error(error)
+  }
+}
+
+const logout = async () => {
+  const { error } = await sb.auth.signOut()
+
+  if (error) {
+    console.error(error)
+  }
+}
+</script>
--- pages/course.vue
+++ pages/course.vue
@@ -1,46 +1,51 @@
 <template>
-  <div class="prose mb-12">
-    <h1>
-      <span class="font-medium">
-        Course:
-        <span class="font-bold">Mastering Nuxt 3</span>
-      </span>
-    </h1>
-  </div>
-
-  <div class="flex flex-row justify-center flex-grow">
-    <div class="prose mr-4 p-10 bg-emerald-50 rounded-3xl min-w-[20ch] max-w-[30ch] flex flex-col">
-      <h3 class="text-xl">Chapters</h3>
-      <template v-for="chapter in chapters" :key="chapter.slug">
-        <h4>{{ chapter.title }}</h4>
-        <template v-for="(lesson, index) in chapter.lessons" :key="lesson.slug">
-          <NuxtLink
-            :to="lesson.path"
-            :class="{ 'bg-white': lesson.path === $route.fullPath }"
-            class="group -mx-4 mb-1 py-1 px-4 no-underline rounded-xl hover:bg-white"
-          >
-            <span class="text-emerald-600 group-hover:text-emerald-800">{{ `${index + 1}. ` }}</span>
-            <span class="group-hover:text-emerald-900">{{ lesson.title }}</span>
-          </NuxtLink>
-        </template>
-      </template>
+  <div>
+    <div class="mb-4 flex justify-between items-center w-full">
+      <div class="prose">
+        <h1>
+          <span class="font-medium">
+            Course:
+            <span class="font-bold">Mastering Nuxt 3</span>
+          </span>
+        </h1>
+      </div>
+      <account-card />
     </div>
 
-    <div class="prose p-10 bg-white rounded-3xl">
-      <NuxtErrorBoundary>
-        <NuxtPage />
-        <template #error="{ error }">
-          <h3>Something went wrong with the lesson 😿</h3>
-          <code>{{ error.value }}</code>
-          <br />
-          <button
-            class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 hover:bg-orange-200 hover:text-orange-800 mt-6"
-            @click="resolveError(error)"
-          >
-            Resolve
-          </button>
+    <div class="flex flex-row justify-center flex-grow">
+      <div class="prose mr-4 p-10 bg-emerald-50 rounded-3xl min-w-[20ch] max-w-[30ch] flex flex-col">
+        <h3 class="text-xl">Chapters</h3>
+        <template v-for="chapter in chapters" :key="chapter.slug">
+          <h4>{{ chapter.title }}</h4>
+          <template v-for="(lesson, index) in chapter.lessons" :key="lesson.slug">
+            <NuxtLink
+              :to="lesson.path"
+              :class="{ 'bg-white': lesson.path === $route.fullPath }"
+              class="group -mx-4 mb-1 py-1 px-4 no-underline rounded-xl hover:bg-white"
+            >
+              <span class="text-emerald-600 group-hover:text-emerald-800">{{ `${index + 1}. ` }}</span>
+              <span class="group-hover:text-emerald-900">{{ lesson.title }}</span>
+            </NuxtLink>
+          </template>
         </template>
-      </NuxtErrorBoundary>
+      </div>
+
+      <div class="prose p-10 bg-white rounded-3xl">
+        <NuxtErrorBoundary>
+          <NuxtPage />
+          <template #error="{ error }">
+            <h3>Something went wrong with the lesson 😿</h3>
+            <code>{{ error.value }}</code>
+            <br />
+            <button
+              class="bg-orange-300 text-orange-900 rounded-xl px-4 py-1 hover:bg-orange-200 hover:text-orange-800 mt-6"
+              @click="resolveError(error)"
+            >
+              Resolve
+            </button>
+          </template>
+        </NuxtErrorBoundary>
+      </div>
     </div>
   </div>
 </template>
