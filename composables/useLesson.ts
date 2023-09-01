3ab632f9a9ca0fbcf6dbd3c96c1f0bd3def9f42b
Christian Krebel <krebel@smartsquare.de>
2023-09-01 19:30:16 +0200

    Add session storage cache for lesson data

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,14 +1,26 @@
 import { RouteParamValue } from "vue-router"
+import { StorageSerializers } from "@vueuse/core"
+import { LessonWithPath } from "~/types/course"
 
 export default async (chaptersSlug: string | RouteParamValue[], lessonsSlug: string | RouteParamValue[]) => {
-  const { data, error } = await useFetch(`/api/course/chapters/${chaptersSlug}/lessons/${lessonsSlug}`)
+  const url = `/api/course/chapters/${chaptersSlug}/lessons/${lessonsSlug}`
+  const lesson = useSessionStorage<LessonWithPath>(url, null, { serializer: StorageSerializers.object })
 
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      message: `Could not fetch lesson ${lessonsSlug} in chapter ${chaptersSlug}.`,
-    })
+  if (!lesson.value) {
+    const { data, error } = await useFetch<LessonWithPath>(url)
+
+    if (error.value) {
+      throw createError({
+        ...error.value,
+        message: `Could not fetch lesson ${lessonsSlug} in chapter ${chaptersSlug}.`,
+      })
+    }
+
+    // Update lesson from session storage
+    lesson.value = data.value
+  } else {
+    console.log(`Getting lesson ${lessonsSlug} in chapter ${chaptersSlug} from cache.`)
   }
 
-  return data
+  return lesson
 }
