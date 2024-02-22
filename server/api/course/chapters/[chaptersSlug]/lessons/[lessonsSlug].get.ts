65d865eb763ff28d1f1747c659d5f2387f6e3917
Christian Krebel <krebel@smartsquare.de>
2024-02-22 19:17:30 +0100

    Replace local lesson data with db fetch

--- server/api/course/chapters/[chaptersSlug]/lessons/[lessonsSlug].get.ts
+++ server/api/course/chapters/[chaptersSlug]/lessons/[lessonsSlug].get.ts
@@ -1,29 +1,29 @@
-import { Chapter, Lesson, LessonWithPath } from "~/types/course"
-import course from "~/server/courseData"
+import { PrismaClient } from "@prisma/client"
+import { LessonWithPath } from "~/types/course"
 
-export default defineEventHandler((event): LessonWithPath => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
   const { chaptersSlug, lessonsSlug } = event.context.params
 
-  const chapter: Option<Chapter> = course.chapters.find((it) => it.slug === chaptersSlug)
-  if (!chapter) {
-    throw createError({
-      statusCode: 404,
-      message: "Chapter not found.",
-    })
-  }
+  const lesson = await prisma.lesson.findFirst({
+    where: {
+      slug: lessonsSlug,
+      chapter: {
+        slug: chaptersSlug,
+      },
+    },
+  })
 
-  const lesson: Option<Lesson> = chapter.lessons.find((it) => it.slug === lessonsSlug)
   if (!lesson) {
     throw createError({
       statusCode: 404,
-      message: "Lesson not found.",
+      statusMessage: "Lesson not found",
     })
   }
 
   return {
     ...lesson,
-    path: `/course/chapters/${chapter.slug}/lessons/${lesson.slug}`,
+    path: `/course/chapters/${chaptersSlug}/lessons/${lessonsSlug}`,
   }
 })
