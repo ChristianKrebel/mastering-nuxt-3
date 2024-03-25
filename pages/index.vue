ec82c6e2128614f4a8574a036c3e7dafbdda5562
Christian Krebel <krebel@smartsquare.de>
2024-03-25 18:19:40 +0100

    Fix routing

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
 const user = useSupabaseUser()
 
 const router = useRouter()
-if (user) {
-  router.push("/course")
+if (user.value) {
+  router.push("/course/chapters/1-chapter-1/lessons/1-introduction-to-typescript-with-vue-js-3")
 } else {
   router.push("/landing")
 }
