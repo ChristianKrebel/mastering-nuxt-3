f6128ba32e91a8616e3b8e150cc965643b2c05be
Christian Krebel <krebel@smartsquare.de>
2024-03-26 11:29:45 +0100

    Prerender the landing page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import eslintPlugin from "vite-plugin-eslint"
 
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ["/landing"],
+    },
+  },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/supabase", "@pinia/nuxt"],
   vite: {
     plugins: [eslintPlugin()],
